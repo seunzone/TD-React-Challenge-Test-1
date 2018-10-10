c5c54baa7c21baf0acea581405083ab597714524
seunzone <darealseun@gmail.com>
2018-10-10 17:04:43 +0100

    Updates

---
+++ precache-manifest.0e18365906e3e7bbbda5728cb24d70d3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "64b9b49f075783ba3c65",
+    "url": "/TD-React-Challenge-Test-1/static/css/main.c2378d0e.chunk.css"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "426aa3909d5e33b1d5dd",
+    "url": "/TD-React-Challenge-Test-1/static/js/1.426aa390.chunk.js"
+  },
+  {
+    "revision": "0d868d1c74a756ad1658",
+    "url": "/TD-React-Challenge-Test-1/static/js/runtime~main.0d868d1c.js"
+  },
+  {
+    "revision": "64b9b49f075783ba3c65",
+    "url": "/TD-React-Challenge-Test-1/static/js/main.64b9b49f.chunk.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/TD-React-Challenge-Test-1/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "426aa3909d5e33b1d5dd",
+    "url": "/TD-React-Challenge-Test-1/static/css/1.10707a83.chunk.css"
+  },
+  {
+    "revision": "b4ba1d5deed3193294215e3f160105a5",
+    "url": "/TD-React-Challenge-Test-1/index.html"
+  }
+];
