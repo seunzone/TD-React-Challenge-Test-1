48fec893f0c693f6072bd44294531bd0d2b0bff7
seunzone <darealseun@gmail.com>
2018-10-10 16:51:45 +0100

    design markup area for landing page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, Fragment } from 'react';
 import './App.css';
 
 class App extends Component {
+  
   render() {
+  
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Fragment>
+        <div className="work-area">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <h2 className="editable">Input (editable field)</h2>
+                <textarea rows="30" cols="60" placeholder="lorem ipsum" />
+              </div>
+              <div className="col-md-6">
+                <h2 className="non-editable">Output</h2>
+                <textarea rows="30" cols="60" placeholder="lorem ipsum"  />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Fragment>
     );
   }
 }
